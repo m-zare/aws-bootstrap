0b6f8d6f2d0b91692b20de7f66f49c11e7ff1442
m-zare <mostafazare@gmail.com>
2021-02-21 23:33:11 +0330

    Add ASG Remove explicitly created EC2 instances

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -55,5 +55,5 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?ends_with(Name,'AWSEndpoint')].Value"
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
 fi
